4fba72286d5d167257a9d33dbf68c022cb7c1cec
GitLab CI <vikrantsingh@geekyants.com>
2025-01-06 11:27:31 +0530

    [ci skip] Bump version code to 6 and version name to 2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.geekyants.synchronoss"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.0.3"
+        versionCode = 6
+        versionName = "2.0.4"
 
 
 
